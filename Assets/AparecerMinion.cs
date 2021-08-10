63752b1dcf98da5333e0d19d3505923903004fb9
DESKTOP-TS22UMM\RAUL <akeymi0.0@gmail.com>
2021-08-10 18:30:01 -0500

    Cosas ui

--- Assets/AparecerMinion.cs
+++ Assets/AparecerMinion.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class WinImprovisado : MonoBehaviour
+public class AparecerMinion : MonoBehaviour
 {
     [SerializeField] GameObject cosa = default;
+    [SerializeField] GameObject minionItem = default;
 
     private void Update()
     {
         if(cosa == null)
         {
-            SceneManager.LoadScene(0);
+            minionItem.SetActive(true);
+            Destroy(this.gameObject);
         }
     }
 }
