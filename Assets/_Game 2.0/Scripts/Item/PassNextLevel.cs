d80f5b4f5616a13a7400b0aa3863b9ca0889c28f
DESKTOP-TS22UMM\RAUL <akeymi0.0@gmail.com>
2021-08-20 01:03:56 -0500

    Puerta siguiente nivel

---
+++ Assets/_Game 2.0/Scripts/Item/PassNextLevel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "Effect_Name", menuName = "RoboTroop/Effects/PassScene", order = 1)]
+public class PassNextLevel : Effect
+{
+    [SerializeField] int sceneNumber = 1;
+
+    public override void Apply()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
