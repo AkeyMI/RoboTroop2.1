2fe572d08dd2a839b0ee306054e351e513d60687
Yissus1945 <garciacantu.jesus@gmail.com>
2021-10-12 19:00:36 -0500

    struct added to spawner pool

--- Assets/_Game 2.0/Scripts/Destructible Objects/SpawnerPool.cs
+++ Assets/_Game 2.0/Scripts/Destructible Objects/SpawnerPool.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class SpawnerPool : MonoBehaviour
 {
-    
-    [SerializeField] GameObject[] particulas;
-    [SerializeField] float[] lifeTimeParticles;
+    [SerializeField] SpawnData[] spData;
 
     private void Awake()
     {
-        foreach (GameObject go in particulas)
+        for (int i = 0; i < spData.Length; i++)
         {
-            if(go != null)
-                ObjectPooling.PreLoad(go, 1);
+            if(spData[i].prefab !=null)
+                ObjectPooling.PreLoad(spData[i].prefab , 1);
         }
+
+        //foreach (GameObject go in )
+        //{
+        //   if(go != null)
+        //        ObjectPooling.PreLoad(go, 1);
+        //}
     }
 
     public void GetParticle(int i , Vector3 t)
     {
-        GameObject c = ObjectPooling.GetObj(particulas[i]);
+        GameObject c = ObjectPooling.GetObj(spData[i].prefab);
         c.transform.position = t;
         c.GetComponentInChildren<ParticleSystem>().Play();
-        StartCoroutine (Despawn(particulas[i], c, lifeTimeParticles[i]));
+        StartCoroutine (Despawn(spData[i].prefab, c, spData[i].timeDestroy));
     }
 
     IEnumerator Despawn(GameObject prefab,GameObject instance , float i)
@@ -31,4 +35,11 @@ public class SpawnerPool : MonoBehaviour
         ObjectPooling.ReObj(prefab, instance);
     }
 
+}
+
+[System.Serializable]
+public struct SpawnData
+{
+    public GameObject prefab;
+    public float timeDestroy;
 }
