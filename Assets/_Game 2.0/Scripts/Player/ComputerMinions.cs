718ca82fade65ba2e36fb6646198036224bbc7b6
DESKTOP-TS22UMM\RAUL <akeymi0.0@gmail.com>
2021-10-25 00:54:37 -0500

    Computer funcionando correctamente

--- Assets/_Game 2.0/Scripts/Player/ComputerMinions.cs
+++ Assets/_Game 2.0/Scripts/Player/ComputerMinions.cs
@@ -1,19 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ComputerMinions : MonoBehaviour
 {
+    [SerializeField] float distanceToActivate = 2f;
+    [SerializeField] Image eImage = default;
+    [SerializeField] GameObject boxMinions = default;
+
+    private bool isClose;
+
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.U))
+        CheckIfThereIsPlayer();
+
+        if (Input.GetKeyDown(KeyCode.E) && isClose)
         {
             ActivateComputer();
         }
     }
 
+    private void CheckIfThereIsPlayer()
+    {
+        Collider[] players = Physics.OverlapSphere(this.transform.position, distanceToActivate);
+        foreach (var player in players)
+        {
+            if (player.CompareTag("Player") || player.CompareTag("AtkMinion"))
+            {
+                isClose = true;
+                if (eImage != null)
+                    eImage.enabled = true;
+
+            }
+            else
+            {
+                isClose = false;
+                if (eImage != null)
+                    eImage.enabled = false;
+
+            }
+        }
+    }
+
+    public void DesactivateBoxMinions()
+    {
+        boxMinions.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
     public void ActivateComputer()
     {
+        boxMinions.SetActive(true);
+        Time.timeScale = 0f;
         MinionController minionController = FindObjectOfType<MinionController>();
         BoxMInion boxMInion = FindObjectOfType<BoxMInion>();
         boxMInion.CreateBoxElements(minionController.GetMinionBoxDataList());
