ef3761e061599c710ffe1f4501f9fe2c4b023563
DESKTOP-TS22UMM\RAUL <akeymi0.0@gmail.com>
2021-11-10 21:11:52 -0600

    Listo el random item de cada room

--- Assets/AparecerMinion.cs
+++ Assets/AparecerMinion.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class AparecerMinion : MonoBehaviour
 {
     [SerializeField] GameObject cosa = default;
-    [SerializeField] GameObject minionItem = default;
+    [SerializeField] GameObject[] minionItem = default;
 
     private void Update()
     {
         if(cosa == null)
         {
-            minionItem.SetActive(true);
+            minionItem[Random.Range(0, minionItem.Length - 1)].SetActive(true);
             Destroy(this.gameObject);
         }
     }
